e795a894ef17cbfbdb42a36901eddd4f9700e1b4
InHiBiToRs <tanakrit64566@gmail.com>
2023-02-10 16:06:01 +0700

    lab17_1_complete

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,44 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *a,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << a[i];
+        if(i%M == M-1) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *a,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        a[i] = (rand()%101)/100.0;
+    }
+}
+
+void findRowSum(double *a,double *b,int N,int M){
+    double sum1 = 0;
+    int j = 0;
+	for(int i = 0; i < N*M; i++){
+        sum1 += *(a+i);
+    	if(i%M == M-1){
+    	    b[j] = sum1;
+    	    sum1 = 0;
+    	    j++;
+    	}
+    }
+
+}
+
+void findColSum(double *a,double *b,int N,int M){
+    double sum2 = 0;
+	for(int i = 0; i < M; i++){
+        for(int j = 0; j < N; j++){
+            sum2 += *(a+(j*M)+i);
+        }
+        b[i] = sum2;
+        sum2 = 0;
+    }
+
+}
